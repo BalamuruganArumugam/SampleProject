9ce65c3d473d260cb7020589c84df10086c0f4fc
balamurugan <balaarumugam2151993@gmail.com>
2022-05-07 21:33:30 +0530

    Sample code is added

---
+++ src/org/college/College.java
@@ -0,0 +1,35 @@
+package org.college;
+
+public class College {
+	private void collegeName() {
+	System.out.println("SASTRA");
+}
+	private void collegeCode() {
+		System.out.println(100);
+
+	}	
+	private void collegeRank() {
+		System.out.println("Third Rank");
+
+	}
+	
+	public static void main(String[] args) {
+		College c = new College();
+		c.collegeName();
+		c.collegeCode();
+		c.collegeRank();
+		
+		Student s = new Student();
+		s.studentName();
+		s.studentDept();
+		s.studentId();
+		
+		Hostel h = new Hostel();
+		h.hostelName();
+		
+		Dept d = new Dept();
+		d.deptName();
+	
+		
+	}
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,15 @@
+package org.college;
+
+public class Student {
+	public void studentName() {
+		System.out.println("Ram");
+
+	}
+	public void studentDept() {
+	System.out.println("CS");
+
+	}
+	public void studentId() {
+		System.out.println(108060);
+	}
+}
